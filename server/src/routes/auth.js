1c83ced93884088d5b71220f37b73ce2d05f1a73
Anh Thu <nguyenanhthu09205@gmail.com>
2026-03-30 14:05:56 +0700

    Thêm chức năng đăng nhập bằng tài khoản gg

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
-const { register, login, forgotPassword, resetPassword } = require('../controllers/authController');
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:id/:token', resetPassword);
+const router = require("express").Router();
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
+const { googleLogin } = require("../controllers/authController");
+router.post("/register", register);
+router.post("/login", login);
+router.post("/google", googleLogin);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:id/:token", resetPassword);
 
 module.exports = router;
