86b1c333e7415ba76c21e21052577e9967cdd039
NhiBuaa <nhihuynh210905@gmail.com>
2026-05-26 20:36:09 +0700

    Make Firebase admin config optional for CI

--- server/src/config/firebaseAdmin.js
+++ server/src/config/firebaseAdmin.js
@@ -1,10 +1,28 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service.json");
 
-if (!admin.apps.length) {
+const loadServiceAccount = () => {
+  try {
+    return require("./firebase-service.json");
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      throw new Error("Firebase Admin credentials are not configured");
+    }
+    throw error;
+  }
+};
+
+const getFirebaseAdmin = () => {
+  if (admin.apps.length) return admin;
+
+  const serviceAccount = loadServiceAccount();
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
 
-module.exports = admin;
+  return admin;
+};
+
+module.exports = {
+  auth: () => getFirebaseAdmin().auth(),
+  getFirebaseAdmin,
+};
